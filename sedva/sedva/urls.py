9d9aed4f1c5120d9f51ea837ef8346b2ff9e6d22
codingGee <codinggeeprophet@gmail.com>
2022-09-14 03:28:09 +0100

    installed django-oscar for E-commerce

--- sedva/sedva/urls.py
+++ sedva/sedva/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.apps import apps
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+    # The Django admin is not officially supported; expect breakage.
+    # Nonetheless, it's often useful for debugging.
     path('admin/', admin.site.urls),
+
+    path('', include(apps.get_app_config('oscar').urls[0])),
 ]
---
+++ sedva/sedva_shop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SedvaShopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sedva_shop'
